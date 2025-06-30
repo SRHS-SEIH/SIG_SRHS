25920dbf0524aefaa3a311498a64e1dec6171731
SRHS-SEIH <srhs.seih@gmail.comgit>
2025-06-30 10:19:40 -0300

    Adicionado labels das barragens, resolvendo erros e mal funcionamento do sistema e melhoria geral de estilos.

--- js/labels.js
+++ js/labels.js
@@ -1,4 +1,6 @@
 var hideLabel = function(label) {
+    label.labelObject.style.opacity = 0;
+    label.labelObject.style.transition = 'opacity 0s';
   if (label && label.style) {
       label.style.opacity = 0;
       label.style.transition = 'opacity 0s';
@@ -6,16 +8,30 @@ var hideLabel = function(label) {
 };
 
 var showLabel = function(label) {
+    label.labelObject.style.opacity = 1;
+    label.labelObject.style.transition = 'opacity 1s';
   if (label && label.style) {
       label.style.opacity = 1;
       label.style.transition = 'opacity 1s';
   }
 };
+labelEngine = new labelgun.default(hideLabel, showLabel);
+
+var id = 0;
+var labels = [];
+var totalMarkers = 0;
 
 // Create label engine
 labelEngine = new labelgun.default(hideLabel, showLabel);
 
 function resetLabels(markers) {
+    labelEngine.reset();
+    var i = 0;
+    for (var j = 0; j < markers.length; j++) {
+        markers[j].eachLayer(function(label){
+            addLabel(label, ++i);
+        });
+    }
   labelEngine.reset();
   var i = 0;
   for (var j = 0; j < markers.length; j++) {
@@ -26,6 +42,40 @@ function resetLabels(markers) {
   labelEngine.update();
 }
 
+function addLabel(layer, id) {
+
+  // This is ugly but there is no getContainer method on the tooltip :(
+  if (layer.getTooltip()) {
+      var label = layer.getTooltip()._source._tooltip._container;
+      if (label) {
+
+        // We need the bounding rectangle of the label itself
+        var rect = label.getBoundingClientRect();
+
+        // We convert the container coordinates (screen space) to Lat/lng
+        var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
+        var topRight = map.containerPointToLatLng([rect.right, rect.top]);
+        var boundingBox = {
+          bottomLeft : [bottomLeft.lng, bottomLeft.lat],
+          topRight   : [topRight.lng, topRight.lat]
+        };
+
+        // Ingest the label into labelgun itself
+        labelEngine.ingestLabel(
+          boundingBox,
+          id,
+          parseInt(Math.random() * (5 - 1) + 1), // Weight
+          label,
+          "Test " + id,
+          false
+        );
+
+        // If the label hasn't been added to the map already
+        // add it and set the added flag to true
+        if (!layer.added) {
+          layer.addTo(map);
+          layer.added = true;
+        }
 function addLabelToEngine(layer, id) {
   const tooltip = layer.getTooltip && layer.getTooltip();
   if (!tooltip) return;
@@ -66,3 +116,6 @@ function addLabelToEngine(layer, id) {
       false
   );
 }
+ } 
+   }
+     }
